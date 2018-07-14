89246d267ff2d75f24a4817160499eda6fb3e847
sherry-du <726845321@qq.com>
2018-07-14 09:20:12 +0800

    ex04

---
+++ ex04/ex04.hpp
@@ -0,0 +1,18 @@
+#ifndef EX04_
+#define EX04_
+
+#include <iostream>
+
+template <typename T>
+class Tester
+{
+public:
+  Tester();
+  ~Tester();
+  bool equal(T const& a, T const& b);
+};
+
+template <typename T>
+bool equal(T const& a, T const& b);
+
+#endif
